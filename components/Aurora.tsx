9b5145d97d8546f44ff14106d40f0c29f6bc1c0a
bhaskar <bn7740401@gmail.com>
2026-09-19 15:53:31 +0530

    Add reference-style aurora background

---
+++ components/Aurora.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Aurora() {
+  return (
+    <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden" aria-hidden="true">
+      <div className="reference-aurora reference-aurora-one" />
+      <div className="reference-aurora reference-aurora-two" />
+      <div className="reference-aurora reference-aurora-three" />
+    </div>
+  );
+}
